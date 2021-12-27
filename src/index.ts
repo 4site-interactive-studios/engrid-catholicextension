6ef8a351e857f0876021b7555d450353c678777a
bryancasler <bryan.casler@gmail.com>
2021-12-27 12:15:05 -0500

    Update to latest engrid template and engrid-scripts release

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-// import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
-import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
+import { Options, App } from "@4site/engrid-common"; // Uses ENGrid via NPM
+// import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
-import "./scripts/main.js";
+import { customScript } from "./scripts/main";
 
 const options: Options = {
   applePay: false,
   CapitalizeFields: true,
   ClickToExpand: true,
   CurrencySymbol: "$",
-  CurrencySeparator: ".",
+  DecimalSeparator: ".",
+  ThousandsSeparator: ",",
   MediaAttribution: true,
   SkipToMainContentLink: true,
   SrcDefer: true,
-  // ProgressBar: true,
+  ProgressBar: true,
   Debug: App.getUrlParameter("debug") == "true" ? true : false,
-  onLoad: () => console.log("Starter Theme Loaded"),
+  onLoad: () => customScript(),
   onResize: () => console.log("Starter Theme Window Resized"),
 };
 new App(options);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
-document.onreadystatechange = () => {    
-    if(document.readyState === "interactive" || document.readyState === "complete"){
+export const customScript = function () {
+  console.log("ENGrid client scripts are executing");
+  // Add your client scripts here
 
-        // Add placeholder to the "other" giving amount field
-        let enFieldOtherAmt = document.querySelectorAll('.en__field--donationAmt .en__field__item--other .en__field__input--other')[0];
-        if(enFieldOtherAmt){
-            enFieldOtherAmt.placeholder = "Other";
-        }
-
-    }
-}
+  // Add placeholder to the "other" giving amount field
+  let enFieldOtherAmt = document.querySelectorAll(
+    ".en__field--donationAmt .en__field__item--other .en__field__input--other"
+  )[0];
+  if (enFieldOtherAmt) {
+    enFieldOtherAmt.placeholder = "Other";
+  }
+};
